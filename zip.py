7d17ccfba334b0573ce4a54e05891cc42951e754
geordi123 <geordi-gabriel.renaud-dumoulin.1@ens.etsmtl.ca>
2022-11-22 13:27:18 -0500

    commit

--- zip.py
+++ zip.py
@@ -1,5 +1,5 @@
 import zipfile
 import os
 
-with zipfile.ZipFile(".\database.zip", 'r') as zip_ref:
+with zipfile.ZipFile("/home/ens/AP69690/database.zip", 'r') as zip_ref:
     zip_ref.extractall(os.curdir)
